fb61cb2721529201933ce1bda4f99dbc295fb5ab
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-05-30 21:20:21 -0300

    added more urls

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,9 @@ from . import views
 # TODO: adicionar todas as views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name = 'index'),
+    path('vendedor/', views.vendedor, name = 'vendedor'),
+    path('cliente/', views.vendedor, name = 'cliente'),
+    path('produto/', views.vendedor, name = 'produto'),
+    path('venda/', views.vendedor, name = 'venda')
 ]
